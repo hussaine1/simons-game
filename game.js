084b41e3c4bdc0341aadbff5ba7ef7f1828b95c9
hussaine1 <hussainetisham@gmail.com>
2023-11-29 17:19:51 +0000

    changes to javascript

--- game.js
+++ game.js
@@ -1,18 +1,33 @@
-let buttonColours = ["red", "blue", "green", "yellow"];
-let gamePattern = [];
+$(document).on("keypress", function () {
+    $("#level-title").text("Level 1");
+    let buttonColours = ["red", "blue", "green", "yellow"];
+    let gamePattern = [];
 
-// this function generates a random number 
-function nextSequence() {
-    // Math.random() will generate a random number from 0-0.999 (never reaching 1)
-    // Math.floor() will round down to the nearest int
-    // so if i did Math.floor(Math.random()*3) then range of values I can get is 0, 1, 2
-    // but need 0, 1, 2, 3. therefore do *4 
-    let randomNumber = Math.floor(Math.random() * 4);
-    return randomNumber;
-}
+    // this function generates a random number 
+    function nextSequence() {
+        // Math.random() will generate a random number from 0-0.999 (never reaching 1)
+        // Math.floor() will round down to the nearest int
+        // so if i did Math.floor(Math.random()*3) then range of values I can get is 0, 1, 2
+        // but need 0, 1, 2, 3. therefore do *4 
+        let randomNumber = Math.floor(Math.random() * 4);
+        return randomNumber;
+    }
 
-// this statement stores the randomly selected colour from the buttonColours array
-let randomChosenColour = buttonColours[nextSequence()];
+    // this statement stores the randomly selected colour from the buttonColours array
+    let randomChosenColour = buttonColours[nextSequence()];
 
-// this stores the randomly selected colours 
-gamePattern.push(randomChosenColour);
+    // this stores the randomly selected colours 
+    gamePattern.push(randomChosenColour);
+
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+
+    // due to new chrome update you cant just play sounds 
+    // chrome is more strict
+    // either you can autoplay sounds muted
+    // or the sound required the user to interact with the site first, e.g. pressing a button
+
+    let x = randomChosenColour+"Sound";
+
+    document.getElementById(x).play();
+});
+
