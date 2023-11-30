51424f43d1f3f81a26f6c16bd3f39a9a97fed750
hussaine1 <90181696+hussaine1@users.noreply.github.com>
2023-11-30 01:21:44 +0000

    changes to js file

--- game.js
+++ game.js
@@ -1,7 +1,9 @@
 $(document).on("keypress", function () {
-    $("#level-title").text("Level 1");
+    // $("#level-title").text("Level 0");
     let buttonColours = ["red", "blue", "green", "yellow"];
     let gamePattern = [];
+    let userClickedPattern = [];
+    let level = -1;
 
     // this function generates a random number 
     function nextSequence() {
@@ -10,24 +12,42 @@ $(document).on("keypress", function () {
         // so if i did Math.floor(Math.random()*3) then range of values I can get is 0, 1, 2
         // but need 0, 1, 2, 3. therefore do *4 
         let randomNumber = Math.floor(Math.random() * 4);
-        return randomNumber;
+        // this statement stores the randomly selected colour from the buttonColours array
+        let randomChosenColour =buttonColours[randomNumber];
+        // this stores the randomly selected colours 
+        gamePattern.push(randomChosenColour);
+        $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+        playSound(randomChosenColour);
+        level ++;
+        $("#level-title").text("Level "+level);
+        // console.log("test");
     }
 
-    // this statement stores the randomly selected colour from the buttonColours array
-    let randomChosenColour = buttonColours[nextSequence()];
+    nextSequence();
 
-    // this stores the randomly selected colours 
-    gamePattern.push(randomChosenColour);
+    function playSound(name) {
+        
+        document.getElementById(name+"Sound").play();
+    }
 
-    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+    function animatePress(currentColour) {
+        $("."+currentColour).addClass("pressed");
+        setTimeout(function(){
+            $("."+currentColour).removeClass("pressed");
+        }, 100);
+    }
+
+    $(".btn").on("click", function() {
+        let userChosenColour = $(this).attr('id');
+        userClickedPattern.push(userChosenColour);
+        playSound(userChosenColour);
+        animatePress(userChosenColour);
+        // console.log(userClickedPattern);
+    });
 
     // due to new chrome update you cant just play sounds 
     // chrome is more strict
     // either you can autoplay sounds muted
     // or the sound required the user to interact with the site first, e.g. pressing a button
-
-    let x = randomChosenColour+"Sound";
-
-    document.getElementById(x).play();
 });
 
