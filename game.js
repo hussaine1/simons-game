4b53a06a97bc520463297c7e259e15f5fcfe4c9a
hussaine1 <90181696+hussaine1@users.noreply.github.com>
2023-12-11 17:21:44 +0000

    game now complete

--- game.js
+++ game.js
@@ -16,8 +16,12 @@ $(document).on("keypress", function () {
         let randomChosenColour =buttonColours[randomNumber];
         // this stores the randomly selected colours 
         gamePattern.push(randomChosenColour);
-        $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
-        playSound(randomChosenColour);
+
+        for (let i = 1; i <= gamePattern.length; i++)  {
+            $("#"+gamePattern[i-1]).fadeOut(100).fadeIn(100);
+            playSound(gamePattern[i-1]);
+        }
+        
         level ++;
         $("#level-title").text("Level "+level);
         // console.log("test");
@@ -37,13 +41,46 @@ $(document).on("keypress", function () {
         }, 100);
     }
 
+    function checkAnswer(currentLevel) {
+        for (let i=0; i<= currentLevel; i++) {
+            if (userClickedPattern[i] == gamePattern[i]) {
+                console.log("Success");
+            }
+            else {
+                console.log("Wrong");
+                $("body").addClass("game-over");
+                setTimeout(function(){
+                    $("body").removeClass("game-over");
+                }, 200);
+                location.reload();
+            }
+        }
+        setTimeout(function(){
+            console.log("Level Complete!");
+        }, 1000);
+        userClickedPattern=[];
+        
+        nextSequence();
+
+    }
+
     $(".btn").on("click", function() {
         let userChosenColour = $(this).attr('id');
         userClickedPattern.push(userChosenColour);
         playSound(userChosenColour);
         animatePress(userChosenColour);
         // console.log(userClickedPattern);
+        if (userClickedPattern.length == gamePattern.length) {
+            checkAnswer(level);
+        }
     });
+
+    
+
+    
+    
+
+
 
     // due to new chrome update you cant just play sounds 
     // chrome is more strict
